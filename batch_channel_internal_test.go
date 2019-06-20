053d7d93a28f6f3566a9af02c1baa1bd88f2628b
KIMSJ <leesd556@gmail.com>
2019-06-21 01:13:51 +0900

    feat : implements batch_channel

---
+++ batch_channel_internal_test.go
@@ -0,0 +1,42 @@
+package cleisthenes
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBatchChannel_Send(t *testing.T) {
+	batchChan := NewBatchChannel()
+
+	member := Member{Address: Address{Ip: "127.0.0.1", Port: 8080}}
+	batch := map[Member][]byte{
+		member: []byte("batch data"),
+	}
+
+	batchChan.Send(BatchMessage{
+		Batch: batch,
+	})
+
+	msg := <-batchChan.buffer
+	if !reflect.DeepEqual(msg.Batch, batch) {
+		t.Fatalf("expected batch size is %d, but got %d", len(batch), len(msg.Batch))
+	}
+}
+
+func TestBatchChannel_Receive(t *testing.T) {
+	batchChan := NewBatchChannel()
+
+	member := Member{Address: Address{Ip: "127.0.0.1", Port: 8080}}
+	batch := map[Member][]byte{
+		member: []byte("batch data"),
+	}
+
+	batchChan.buffer <- BatchMessage{
+		Batch: batch,
+	}
+
+	msg := <-batchChan.Receive()
+	if !reflect.DeepEqual(msg.Batch, batch) {
+		t.Fatalf("expected batch size is %d, but got %d", len(batch), len(msg.Batch))
+	}
+}
